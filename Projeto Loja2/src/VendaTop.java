91d050e1e92fa80a21bef44dccd0835a2bb48719
Yasrq <102964657+Yasrq@users.noreply.github.com>
2022-05-16 08:27:58 -0300

    Add files via upload

---
+++ Projeto Loja2/src/VendaTop.java
@@ -0,0 +1,39 @@
+
+public class VendaTop {
+
+	public static void main(String[] args) {
+		
+		
+		
+		 int jaqueta = 80;
+		 int camiseta = 40; 
+		 int vestido = 50;
+		 int calça = 70;
+		 int desconto;
+		 int valor1;
+		 int valor2; 
+		 
+		 
+		 
+		 valor1 = jaqueta + vestido; 
+		 System.out.println("O valor da compra é " + valor1);
+		 
+		 valor2 = calça + camiseta; 
+		 System.out.println("O valor da compra é " + valor2);
+		 
+		 if ()
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		
+		
+	}
+
+}
