f10f2479583dc3daf145f11ba153fa4a0b28ebca
Yasrq <102964657+Yasrq@users.noreply.github.com>
2022-05-16 08:28:27 -0300

    Add files via upload

---
+++ ProjetoLoja/src/ClasseVenda.java
@@ -0,0 +1,26 @@
+
+public class ClasseVenda {
+
+	public static void main(String[] args) {
+	
+	double prod1 = 250;
+	double prod2 = 235; 
+	double total; 
+	double valor;
+	double desconto;  
+	
+	total = prod1 + prod2; 
+	desconto = total * 0.15;
+	valor = total - desconto; 
+	
+	System.out.println("O valor a pagar é " + valor);
+	
+	
+		
+		
+		
+		
+		
+	}
+
+}
