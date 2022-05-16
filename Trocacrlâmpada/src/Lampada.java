2a94fc652421b1236fb70f77e0b4eaeff144ce70
Yasrq <102964657+Yasrq@users.noreply.github.com>
2022-05-16 08:28:49 -0300

    Add files via upload

---
+++ Trocacrlâmpada/src/Lampada.java
@@ -0,0 +1,46 @@
+
+public class Lampada {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Olá Mundo!"); 
+	System.out.println("Pegue uma escada");
+	System.out.println("Suba a escada");
+	System.out.println("Tire a lâmpada");
+	System.out.println("Desça a escada");
+	System.out.println("Pegue a lâmpada nova");
+	System.out.println("Suba a escada");
+	System.out.println("Troque a lâmpada");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}	
+	
+}
