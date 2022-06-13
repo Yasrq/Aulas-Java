c947bb4f910a7ffb17fa85c44d24e9eaef749329
Yasrq <102964657+Yasrq@users.noreply.github.com>
2022-06-13 17:08:18 -0300

    Update Aplicacao.java

--- SomativaJava/src/Aplicacao.java
+++ SomativaJava/src/Aplicacao.java
@@ -8,21 +8,21 @@ public class Aplicacao {
 			FormaPag valor3 = FormaPag.CARTAO_CREDITO;
 			
 		
-			
+			//estanciar objeto//
 			Usuario usuario = new Usuario();
 			
-			Professor professor1 = new Professor("JosÈ");
+			Professor professor1 = new Professor("Jos√©");
 			Professor professor2 = new Professor("Augusto");
 			
-			Curso curso1 = new Curso("Mec‚nica");
+			Curso curso1 = new Curso("Mec√¢nica");
 			Curso curso2 = new Curso("Eletro");
-	
-			Aluno aluno1 = new Aluno("Manoel", "555555", "Est· matriculado no curso de mec‚nica", "Seu professor ser· o JosÈ");
-			Aluno aluno2 = new Aluno("Rafael", "666666", "Est· matriculado no curso de mec‚nica", "Seu professor ser· o JosÈ");
-			Aluno aluno3 = new Aluno("Carlos", "77777", "Est· matriculado no curso eletro", "Seu professor ser· o Augusto");
-			Aluno aluno4 = new Aluno("Danilo", "888888", "Est· matriculado no curso eletro", "Seu professor ser· o Augusto");
+	                //construtor//
+			Aluno aluno1 = new Aluno("Manoel", "555555", "Est√° matriculado no curso de mec√¢nica", "Seu professor ser√° o Jos√©");
+			Aluno aluno2 = new Aluno("Rafael", "666666", "Est√° matriculado no curso de mec√¢nica", "Seu professor ser√° o Jos√©");
+			Aluno aluno3 = new Aluno("Carlos", "77777", "Est√° matriculado no curso eletro", "Seu professor ser√° o Augusto");
+			Aluno aluno4 = new Aluno("Danilo", "888888", "Est√° matriculado no curso eletro", "Seu professor ser√° o Augusto");
 			
-			List<Usu·rio> listaDeUsuarios 
+			List<Usu√°rio> listaDeUsuarios 
 			
 			
 		
