33bec1592f60f6748683c666775853264c0d1d52
Yasrq <102964657+Yasrq@users.noreply.github.com>
2022-06-13 16:56:14 -0300

    Update Lampada.java

--- Trocacrlâmpada/src/Lampada.java
+++ Trocacrlâmpada/src/Lampada.java
@@ -4,14 +4,15 @@ public class Lampada {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("Ol· Mundo!"); 
+//para aparecer na tela(console)//
+	System.out.println("Ol√° Mundo!"); 
 	System.out.println("Pegue uma escada");
 	System.out.println("Suba a escada");
-	System.out.println("Tire a l‚mpada");
-	System.out.println("DesÁa a escada");
-	System.out.println("Pegue a l‚mpada nova");
+	System.out.println("Tire a l√¢mpada");
+	System.out.println("Des√ßa a escada");
+	System.out.println("Pegue a l√¢mpada nova");
 	System.out.println("Suba a escada");
-	System.out.println("Troque a l‚mpada");
+	System.out.println("Troque a l√¢mpada");
 	
 	
 	
